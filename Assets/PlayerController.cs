520916324c2da204ef76d60ede058623d745836a
walk2347 <walk2347@gmail.com>
2022-05-31 16:44:27 +0900

    asset 폴더 추가

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    public float speed = 8f;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        /*//윗쪽 방향키 +z
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+        //아래쪽 방향키 -z
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+        //오른쪽 방향키 +x
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+        //왼쪽 방향키 -x
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }*/
+
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+
+        playerRigidbody.velocity = newVelocity;
+    }
+
+    public void Die()
+    {
+        //자신의 게임 오브젝트 비활성화
+        gameObject.SetActive(false);
+    }
+}
